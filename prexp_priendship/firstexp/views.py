7eaf6aa8778bf812e3a72680b86ec978d56c3556
todo <todo@sparcs.org>
2016-08-04 14:45:56 +0900

    lots of things changed

--- prexp_priendship/firstexp/views.py
+++ prexp_priendship/firstexp/views.py
@@ -15,7 +15,7 @@ q_list = [Question("친하", "green"), Question("안 친하", "red")]
 def reg_db(request):
 	for p in Politician.objects.all():
 		p.delete()
-	for line in open("utf8_mod_unified_assembly.txt", "r"):
+	for line in open("utf8_mod_unified_assembly_50.txt", "r"):
 		ll = line.split("\t")
 		_name = ll[0]
 		_photo = ll[7]
@@ -28,14 +28,22 @@ def front(request):
 	return render(request, "front.html")
 
 def start(request):
+	# how many did a user solve
+	num_of_sol = 0
+
 	if request.method == "POST":
 		print(request.POST)
+		# log save
 		_token = request.POST["csrfmiddlewaretoken"]
 		_q_kind = request.POST["q_kind"]
 		_shown_list = request.POST["shown_p"]
 		_select_list = request.POST["select_p"]
 		new_log = SubmitLog(token=_token, q_kind=_q_kind, shown_list=_shown_list, select_list=_select_list)
 		new_log.save()
-
+		# num_of_sol update
+		log_list = SubmitLog.objects.filter(token=_token)
+		num_of_sol = len(log_list)
+	
+	# random sort
 	p_list = Politician.objects.all().order_by("?")
-	return render(request, "start.html", {"rp_list": p_list[:6], "q_kind": q_list[randrange(0, 2)]})
+	return render(request, "start.html", {"rp_list": p_list[:6], "q_kind": q_list[randrange(0, 2)], "nos": num_of_sol})
