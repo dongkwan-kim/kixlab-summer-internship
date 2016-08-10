8138f0a217f39980970bd1267b41f3cea5ca3af1
todo <todo@sparcs.org>
2016-08-10 22:23:08 +0900

    improved py_newsapi

--- prexp_priendship/secondexp/py_newsapi.py
+++ prexp_priendship/secondexp/py_newsapi.py
@@ -8,14 +8,14 @@ class NaverNews():
 		self.client_secret = "Q2vDiQF2NA"
 		self.query = query
 
-	def get_response(sort=""):
+	def get_response(self, sort="sim"):
 		base_url = "https://openapi.naver.com/v1/search/news.xml"
-		base_url += "?query="+str(self.query)
-		if sort != "":
-			base_url += "&sort="+str(sort)
+		base_url += "?query="+urllib.parse.quote(str(self.query))
+		base_url += "&sort="+str(sort)
 		
 		request = urllib.request.Request(base_url)
 		request.add_header("X-Naver-Client-Id", self.client_id)
 		request.add_header("X-Naver-Client-Secret", self.client_secret)
-	
+		
+		# this must be decoded to utf-8
 		return urllib.request.urlopen(request)		
