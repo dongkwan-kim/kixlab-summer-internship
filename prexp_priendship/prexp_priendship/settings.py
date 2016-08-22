793b25946b0fb3e016cf3a2af2d7381cfe857fdd
todo <todo@sparcs.org>
2016-08-22 16:45:36 +0900

    start fourthexp

--- prexp_priendship/prexp_priendship/settings.py
+++ prexp_priendship/prexp_priendship/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'adminapp',
 	'firstexp',
 	'secondexp',
-	'adminapp',
+	'thirdexp',
+	'fourthexp',
 )
 
 MIDDLEWARE_CLASSES = (
